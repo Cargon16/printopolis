fdaa98c812a941a4b13764b7cdee4f1802669b6e
Cargon16 <cargon16@ucm.es>
2021-03-08 19:17:41 +0100

    Estructura del proyecto creada

---
+++ src/main/java/es/ucm/fdi/iw/g06/printopolis/PrintopolisApplication.java
@@ -0,0 +1,13 @@
+package es.ucm.fdi.iw.g06.printopolis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintopolisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrintopolisApplication.class, args);
+	}
+
+}
