907e875ad398cfefdae94aa0a9a4150e421e0f4f
CarlosGonzalezTorres <cargon16@ucm.es>
2021-03-09 14:15:08 +0100

    nuevas carpetas creadas 2

---
+++ src/main/java/es/ucm/fdi/iw/g06/printopolis/controller/RootController.java
@@ -0,0 +1,36 @@
+package es.ucm.fdi.iw.g06.printopolis.controller;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Landing-page controller
+ * 
+ * @author mfreire
+ */
+@Controller
+public class RootController {
+	
+	private static final Logger log = LogManager.getLogger(RootController.class);
+	private static final String WELCOME = "welcome";
+
+	@GetMapping("/")
+	public String index(Model model) {
+		model.addAttribute(WELCOME, "Hellooo");
+		return "index";
+	}
+	
+	@GetMapping("/chat")
+	public String chat(Model model, HttpServletRequest request) {
+		return "chat";
+	}
+	
+	@GetMapping("/error")
+	public String error(Model model) {
+		return "error";
+	}	
+}
