9d55af338b552d1af67c4d907c4ba25a032d6cee
PopsBourne <anaalava@ucm.es>
2021-03-16 13:55:39 +0100

    JPA

---
+++ src/main/java/es/ucm/fdi/iw/g06/printopolis/model/Design.java
@@ -0,0 +1,50 @@
+package es.ucm.fdi.iw.g06.printopolis.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Design {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	private String category;
+	private float price;
+	private String name;
+	private String description;
+	private int puntuation;
+	
+	@ManyToMany
+	private List<User> client = new ArrayList<>();
+
+	@ManyToMany
+	private List<User> printer = new ArrayList<>();
+
+
+	@OneToMany
+	@JoinColumn(name="design_id")
+	private List<User> designer = new ArrayList<>();
+
+
+
+	@Override
+	public String toString() {
+		return "Design #" + id;
+	}	
+}
