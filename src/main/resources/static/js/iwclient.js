367fd2e6d0f1ab5fee0479222afa0175e7dd4540
Carlos González Torres <45764371+Cargon16@users.noreply.github.com>
2021-04-28 16:29:24 +0200

    Eventos añadidos en tiempo real, funcionando

--- src/main/resources/static/js/iwclient.js
+++ src/main/resources/static/js/iwclient.js
@@ -1,22 +1,22 @@
 /**
  * WebSocket API, which only works once initialized
  */
-const ws = {		
+const ws = {
 
 	/**
 	 * Number of retries if connection fails
 	 */
 	retries: 3,
-		
+
 	/**
 	 * Default action when message is received. 
 	 */
 	receive: (text) => {
 		console.log(text);
 	},
-	
-	headers: {'X-CSRF-TOKEN' : config.csrf.value},
-	
+
+	headers: { 'X-CSRF-TOKEN': config.csrf.value },
+
 	/**
 	 * Attempts to establish communication with the specified
 	 * web-socket endpoint. If successfull, will call 
@@ -26,57 +26,57 @@ const ws = {
 			ws.stompClient = Stomp.client(endpoint);
 			ws.stompClient.reconnect_delay = 2000;
 			// only works on modified stomp.js, not on original from mantainer's site
-			ws.stompClient.reconnect_callback = () => ws.retries -- > 0;
+			ws.stompClient.reconnect_callback = () => ws.retries-- > 0;
 			ws.stompClient.connect(ws.headers, () => {
-		        ws.connected = true;
-		        console.log('Connected to ', endpoint, ' - subscribing...');		        
-		        while (subs.length != 0) {
-		        	ws.subscribe(subs.pop())
-		        }
-		    });			
+				ws.connected = true;
+				console.log('Connected to ', endpoint, ' - subscribing...');
+				while (subs.length != 0) {
+					ws.subscribe(subs.pop())
+				}
+			});
 			console.log("Connected to WS '" + endpoint + "'")
 		} catch (e) {
 			console.log("Error, connection to WS '" + endpoint + "' FAILED: ", e);
 		}
 	},
-	
+
 	subscribe: (sub) => {
-        try {
-	        ws.stompClient.subscribe(sub, 
-	        		(m) => ws.receive(JSON.parse(m.body))); 	// falla si no recibe JSON!
-        	console.log("Hopefully subscribed to " + sub);
-        } catch (e) {
-        	console.log("Error, could not subscribe to " + sub);
-        }
+		try {
+			ws.stompClient.subscribe(sub,
+				(m) => ws.receive(JSON.parse(m.body))); 	// falla si no recibe JSON!
+			console.log("Hopefully subscribed to " + sub);
+		} catch (e) {
+			console.log("Error, could not subscribe to " + sub);
+		}
 	}
-} 
+}
 
 /**
  * Sends an ajax request using fetch
  */
 //envía json, espera json de vuelta; lanza error si status != 200
 function go(url, method, data = {}) {
-  let params = {
-    method: method, // POST, GET, POST, PUT, DELETE, etc.
-    headers: {
-      "Content-Type": "application/json; charset=utf-8",
-    },
-    body: JSON.stringify(data)
-  };
-  if (method === "GET") {
-	  delete params.body;
-  } else {
-      params.headers["X-CSRF-TOKEN"] = config.csrf.value; 
-  }  
-  console.log("sending", url, params)
-  return fetch(url, params)
-  	.then(response => {
-	    if (response.ok) {
-	        return response.json(); // esto lo recibes con then(d => ...)
-	    } else {
-	    	throw response.text();  // esto lo recibes con catch(d => ...)
-	    }
-  	})
+	let params = {
+		method: method, // POST, GET, POST, PUT, DELETE, etc.
+		headers: {
+			"Content-Type": "application/json; charset=utf-8",
+		},
+		body: JSON.stringify(data)
+	};
+	if (method === "GET") {
+		delete params.body;
+	} else {
+		params.headers["X-CSRF-TOKEN"] = config.csrf.value;
+	}
+	console.log("sending", url, params)
+	return fetch(url, params)
+		.then(response => {
+			if (response.ok) {
+				return response.json(); // esto lo recibes con then(d => ...)
+			} else {
+				throw response.text();  // esto lo recibes con catch(d => ...)
+			}
+		})
 }
 
 /**
@@ -84,11 +84,18 @@ function go(url, method, data = {}) {
  */
 document.addEventListener("DOMContentLoaded", () => {
 	if (config.socketUrl) {
-		let subs = config.admin ? 
-				["/topic/admin", "/user/queue/updates", "/topic/printer"] : ["/user/queue/updates", "/topic/printer"]
+		let subs;
+		console.log(config.admin);
+		if (config.admin == true){
+			subs = ["/topic/admin", "/user/queue/updates", "/topic/printer"];
+		}else{
+			subs= ["/user/queue/updates", "/topic/printer"];
+			console.log(subs);
+		} 
+		
 		ws.initialize(config.socketUrl, subs);
 	}
-	
+
 	// add your after-page-loaded JS code here; or even better, call 
 	// 	 document.addEventListener("DOMContentLoaded", () => { /* your-code-here */ });
 	//   (assuming you do not care about order-of-execution, all such handlers will be called correctly)
